db18c28d4b46af1aed8fdad27071d778976a5209
Zachkery <zachkery999@gmail.com>
2018-09-04 18:58:58 -0500

    Templates/urls/session
    Added templates index.html/result.html.
    Index to submit a form.
    Result to display the forms' information after being processed in surveys/process route. AKA process() in views.py.
    Session counter and variables created with forms work with no issues.
    Added a reset route/function to reset the counter held in session.
    No issues

--- apps/survey_app/urls.py
+++ apps/survey_app/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns= [
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^surveys/process$', views.process),
+    url(r'^result$', views.result),
+    url(r'reset$', views.reset)
 ]
--- apps/survey_app/views.py
+++ apps/survey_app/views.py
@@ -4,7 +4,27 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    response = 'Hello World'
-    return HttpResponse(response)
+    return render(request,'survey_app/index.html') 
 
+def result(request):
+    return render(request,'survey_app/result.html')
+
+def process(request):
+    # Gather all form data and place into a session key
+    request.session['your_name'] = request.POST['your_name']
+    request.session['dojo_location'] = request.POST['dojo_location']
+    request.session['favorite_language'] = request.POST['favorite_language']
+    request.session['comment'] = request.POST['comment']
+
+    # Creates a counter with initial submit of a survey
+    if not 'counter' in request.session:
+        request.session['counter'] = 1
+        return redirect('/result')
+    # Add one to the counter with every submission 
+    request.session['counter'] += 1
+    return redirect('/result')
+
+def reset(request):
+    request.session['counter'] = 0
+    return redirect('/result')
 
